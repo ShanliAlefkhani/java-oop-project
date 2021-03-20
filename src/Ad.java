83e670dbaca0bcc6867e0b8d034107a20b24b03a
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-03-20 23:18:57 +0330

    Edit Ad's constructor.

--- src/Ad.java
+++ src/Ad.java
@@ -2,8 +2,13 @@ public class Ad extends BaseAdvertising{
     private String title;
     private String imgUrl;
     private String link;
+    private Advertiser advertiser;
 
-    public Ad(int id) {
+    public Ad(int id, String title, String imgUrl, String link, Advertiser advertiser) {
         super(id);
+        this.title = title;
+        this.imgUrl = imgUrl;
+        this.link = link;
+        this.advertiser = advertiser;
     }
 }
