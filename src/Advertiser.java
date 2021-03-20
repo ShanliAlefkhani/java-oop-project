5cd5ead715cc9d6726cd5d3124d253f795cd3777
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-03-20 23:09:59 +0330

    Add Advertiser's functions.

--- src/Advertiser.java
+++ src/Advertiser.java
@@ -1,8 +1,30 @@
 public class Advertiser extends BaseAdvertising{
+    static int totalClicks;
     private String name;
 
     public Advertiser(int id, String name) {
         super(id);
         this.name = name;
+    }
+
+    public static int getTotalClicks() {
+        return totalClicks;
+    }
+    @Override
+    public void incClicks() {
+        super.incClicks();
+        totalClicks++;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String help() {
+        return "Something";
+    }
+    public String describeMe() {
+        return "Something";
     }
 }
