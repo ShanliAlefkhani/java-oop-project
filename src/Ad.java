7c37ac0135c4510f0a5b4ecfbaf08f26193b1ebe
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-03-20 23:23:57 +0330

    Add Ad's functions.

--- src/Ad.java
+++ src/Ad.java
@@ -11,4 +11,30 @@ public class Ad extends BaseAdvertising{
         this.link = link;
         this.advertiser = advertiser;
     }
+
+    @Override
+    public String describeMe() {
+        return null;
+    }
+    public String getTitle() {
+        return this.title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getImgUrl() {
+        return this.imgUrl;
+    }
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+    public String getLink() {
+        return this.link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
+    public void setAdvertiser(Advertiser advertiser) {
+        this.advertiser = advertiser;
+    }
 }
