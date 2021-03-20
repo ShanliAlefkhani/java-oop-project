814b8f01778aaf1da13eefd238c17579a56dd545
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-03-20 22:52:58 +0330

    Add BaseAdvertising's functions.

--- src/BaseAdvertising.java
+++ src/BaseAdvertising.java
@@ -2,4 +2,17 @@ public abstract class BaseAdvertising {
     private int id;
     private int clicks;
     private int views;
+
+    int getClicks() {
+        return this.clicks;
+    }
+    int getViews() {
+        return this.views;
+    }
+    void incClicks() {
+        this.clicks++;
+    }
+    void incViews() {
+        this.views++;
+    }
 }
