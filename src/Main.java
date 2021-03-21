2a7a8ac42b36c0105681f0970a1346d944deb153
ShanliAlefkhani <shanli.alefkhani@gmail.com>
2021-03-21 12:09:03 +0330

    Print outputs.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         Advertiser advertiser2 = new Advertiser(2, "name2");
         Ad ad1 = new Ad(1, "title1", "img-url1", "link1", advertiser1);
         Ad ad2 = new Ad(2, "title2", "img-url2", "link2", advertiser2);
-        BaseAdvertising.describeMe();
-        ad2.describeMe();
-        advertiser1.describeMe();
+        System.out.println(BaseAdvertising.describeMe());
+        System.out.println(ad2.describeMe());
+        System.out.println(advertiser1.describeMe());
         ad1.incViews();
         ad1.incViews();
         ad1.incViews();
@@ -15,12 +15,12 @@ public class Main {
         ad1.incClicks();
         ad1.incClicks();
         ad2.incClicks();
-        advertiser2.getName();
+        System.out.println(advertiser2.getName());
         advertiser2.setName("new name");
-        advertiser2.getName();
-        ad1.getClicks();
-        advertiser2.getClicks();
-        Advertiser.getTotalClicks();
-        Advertiser.help();
+        System.out.println(advertiser2.getName());
+        System.out.println(ad1.getClicks());
+        System.out.println(advertiser2.getClicks());
+        System.out.println(Advertiser.getTotalClicks());
+        System.out.println(Advertiser.help());
     }
 }
